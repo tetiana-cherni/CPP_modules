769a1a3d22dd4795ecef339c5fd7a03a89abeee5
Tetiana Cherniaieva <tetiana.cherni@gmail.com>
2026-02-19 22:03:54 +0100

    refactor: organize tests with SUBCASE for better structure

--- ex01/tests/test_contact.cpp
+++ ex01/tests/test_contact.cpp
@@ -4,78 +4,60 @@
 #include "doctest.h"
 #include "contact.hpp"
 
-TEST_CASE("ContactTests: ConstructorInitialization") {
-	Contact c;
-	
-	CHECK(c.GetFirstName() == "");
-	CHECK(c.GetLastName() == "");
-	CHECK(c.GetNickname() == "");
-	CHECK(c.GetPhoneNumber() == "");
-	CHECK(c.GetDarkestSecret() == "");
-}
+TEST_SUITE("Contact"){
 
-TEST_CASE("ContactTests, SetFirstName") {
-	Contact c;
-	CHECK(c.SetFirstName("John") == Contact::CheckError::Ok);
-	CHECK(c.GetFirstName() == "John");
-}
+	TEST_CASE("ConstructorInitialization") {
+		Contact c;
 
-TEST_CASE("ContactTests, SetFirstNameEmpty") {
-	Contact c;
-	CHECK(c.SetFirstName("") == Contact::CheckError::EmptyLine);
-}
+		CHECK(c.GetFirstName() == "");
+		CHECK(c.GetLastName() == "");
+		CHECK(c.GetNickname() == "");
+		CHECK(c.GetPhoneNumber() == "");
+		CHECK(c.GetDarkestSecret() == "");
+	}
 
-TEST_CASE("ContactTests, SetLastName") {
-	Contact c;
-	CHECK(c.SetLastName("Ride") == Contact::CheckError::Ok);
-	CHECK(c.GetLastName() == "Ride");
-}
+	TEST_CASE("Setters validation and modofocation"){
 
-TEST_CASE("ContactTests, SetLastNameEmpty") {
-	Contact c;
-	CHECK(c.SetLastName("") == Contact::CheckError::EmptyLine);
-}
+		Contact c;
 
-TEST_CASE("ContactTests, SetNickname") {
-	Contact c;
-	CHECK(c.SetNickname("Ukraine") == Contact::CheckError::Ok);
-	CHECK(c.GetNickname() == "Ukraine");
-}
+		SUBCASE("FirstName") {
+			CHECK(c.SetFirstName("John") == Contact::CheckError::Ok);
+			CHECK(c.GetFirstName() == "John");
+			CHECK(c.SetFirstName("") == Contact::CheckError::EmptyLine);
+		}
 
-TEST_CASE("ContactTests, SetNicknameEmpty") {
-	Contact c;
-	CHECK(c.SetNickname("") == Contact::CheckError::EmptyLine);
-}
+		SUBCASE("LastName"){
+			CHECK(c.SetLastName("Ride") == Contact::CheckError::Ok);
+			CHECK(c.GetLastName() == "Ride");
+			CHECK(c.SetLastName("") == Contact::CheckError::EmptyLine);
+		}
 
-TEST_CASE("ContactTests, SetPhoneNumber") {
-	Contact c;
-	CHECK(c.SetPhoneNumber("+3429844678") == Contact::CheckError::Ok);
-	CHECK(c.GetPhoneNumber() == "+3429844678");
-	CHECK(c.SetPhoneNumber("123456789") == Contact::CheckError::Ok);
-	CHECK(c.GetPhoneNumber() == "123456789");
-}
+		SUBCASE("Nickname"){
+			CHECK(c.SetNickname("Ukraine") == Contact::CheckError::Ok);
+			CHECK(c.GetNickname() == "Ukraine");
+			CHECK(c.SetNickname("") == Contact::CheckError::EmptyLine);
+		}
 
-TEST_CASE("ContactTests, SetPhoneNumberEmpty") {
-	Contact c;
-	CHECK(c.SetPhoneNumber("") == Contact::CheckError::EmptyLine);
-}
+		SUBCASE("PhoneNumber"){
+			//valid input
+				CHECK(c.SetPhoneNumber("+3429844678") == Contact::CheckError::Ok);
+				CHECK(c.GetPhoneNumber() == "+3429844678");
+				CHECK(c.SetPhoneNumber("123456789") == Contact::CheckError::Ok);
+				CHECK(c.GetPhoneNumber() == "123456789");
+				
+			// invalid input
+				CHECK(c.SetPhoneNumber("") == Contact::CheckError::EmptyLine);
+				CHECK(c.SetPhoneNumber("-348") == Contact::CheckError::NotNumber);
+				CHECK(c.SetPhoneNumber("12abcd") == Contact::CheckError::NotNumber);
+				CHECK(c.SetPhoneNumber("abcd") == Contact::CheckError::NotNumber);
+				CHECK(c.SetPhoneNumber("12O34") == Contact::CheckError::NotNumber);
+				CHECK(c.SetPhoneNumber("3+48") == Contact::CheckError::NotNumber);
+		}
 
-TEST_CASE("ContactTests, SetPhoneNumberNotNumber") {
-	Contact c;
-	CHECK(c.SetPhoneNumber("-348") == Contact::CheckError::NotNumber);
-	CHECK(c.SetPhoneNumber("12abcd") == Contact::CheckError::NotNumber);
-	CHECK(c.SetPhoneNumber("abcd") == Contact::CheckError::NotNumber);
-	CHECK(c.SetPhoneNumber("12O34") == Contact::CheckError::NotNumber);
-	CHECK(c.SetPhoneNumber("3+48") == Contact::CheckError::NotNumber);
-}
-
-TEST_CASE("ContactTests, SetDarkestSecret"){
-	Contact c;
-	CHECK(c.SetDarkestSecret("DarkestSecret = 42!") == Contact::CheckError::Ok);
-	CHECK(c.GetDarkestSecret() == "DarkestSecret = 42!");
-}
-
-TEST_CASE("ContactTests, SetDarkestSecretEmpty"){
-	Contact c;
-	CHECK(c.SetDarkestSecret("") == Contact::CheckError::EmptyLine);
+		SUBCASE("DarkestSecret"){
+				CHECK(c.SetDarkestSecret("DarkestSecret = 42!") == Contact::CheckError::Ok);
+				CHECK(c.GetDarkestSecret() == "DarkestSecret = 42!");
+				CHECK(c.SetDarkestSecret("") == Contact::CheckError::EmptyLine);
+		}
+	}
 }
