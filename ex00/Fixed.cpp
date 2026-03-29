26aa145e3c122bef084e1f4bbda43f1d9e1e138c
Tetiana Cherniaieva <tetiana.cherni@gmail.com>
2026-03-29 17:40:57 +0200

    feat: add CPP02 with solution of ex00

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,48 @@
+#include "Fixed.hpp"
+#include <string_view>
+//anonimus namespace, visible only in this cpp
+namespace {
+	void	PrintMsg(const std::string_view& msg)
+	{
+		std::cout << msg << std::endl;
+	}
+}
+
+Fixed::Fixed(): fixed_value_ (0){
+	PrintMsg("Default constructor is called");
+};
+
+Fixed::~Fixed(){
+	PrintMsg("Default destructor is called");
+};
+
+Fixed::Fixed(const Fixed& other)
+{
+	PrintMsg("Copy constructor is called");
+	this->fixed_value_ = other.getRawBits();
+	//*this = other;
+}
+
+//always check this isn't self-assignment of class instance
+Fixed& Fixed::operator=(const Fixed& other)
+{
+	PrintMsg("Copy assignment operator is called");
+	if (this != &other)
+	{
+		this->fixed_value_ = other.getRawBits();
+		//*this = other;
+	}
+	return *this;
+}
+
+int Fixed::getRawBits( void ) const
+{
+	PrintMsg("getRawBits member function called");
+	return fixed_value_;
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	PrintMsg("setRawBits member function called");
+	fixed_value_ = raw;
+}
---
+++ ex00/Fixed.hpp
@@ -0,0 +1,29 @@
+
+// https://cppreference.net/cpp/language/as_operator.html
+// https://cppreference.net/cpp/language/classes.html
+// https://www.geeksforgeeks.org/cpp/shallow-copy-and-deep-copy-in-c/
+// https://www.linkedin.com/pulse/understanding-bitwise-copying-c-what-every-developer-should-el-bana-sqa1f/
+// https://stackoverflow.com/questions/42749439/what-is-the-difference-between-memberwise-copy-bitwise-copy-shallow-copy-and-d
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int					fixed_value_;
+		static const int	fraction_value_ = 8;
+	
+	public:
+		Fixed();
+		Fixed(const Fixed& other); //copy constructor
+		Fixed& operator=(const Fixed& other); //copy assignment operator
+		~Fixed();
+	
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+
+};
+
+#endif
