d9aea6056fdc4c2ca41d3652bc45b1bf9daed213
Tetiana Cherniaieva <tetiana.cherni@gmail.com>
2026-04-16 18:32:52 +0200

    refactor: remove unused commits with colors draft

--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -52,13 +52,4 @@ class Fixed {
 
 	std::ostream& operator<<(std::ostream& output_stream, const Fixed& f);
 
-// colors, inline makes only one copy of variable (from standart 17)
-	// static inline const std::string kRed = "\033[1;31m";
-	// static inline const std::string kBlue = "\033[1;34m";
-	// static inline const std::string kGreen = "\033[1;32m";
-	// static inline const std::string kMagenta = "\033[1;35m";
-	// static inline const std::string kCyn = "\e[1;36m";
-	// static inline const std::string kBlack = "\e[0;30m";
-	// static inline const std::string kResetColor = "\033[0m";
-
 #endif
